f339c6a378120ea97baa40f31dd00bdcf3398d28
DavidOrtiz14 <david.ortiz14@upr.edu>
2018-02-24 08:30:25 -0400

    Area Calculator

---
+++ AreaCircula.java
@@ -0,0 +1,22 @@
+ public static void main(String [ ] args) {
+
+
+
+
+
+
+}
+import java.util.Scanner;
+
+public class AreaCircular {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("The radious>>>>>>");
+		double m =sc.nextInt();
+
+	 System.out.println("The area of the circule with this r: "+m+" is: "+Math.PI*m*m);
+		}
+
+}
